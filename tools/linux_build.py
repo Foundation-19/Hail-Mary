99e52deed54e135db0a654bdab7c2ccc6971b6b1
Tich none <tichman27@gmail.com>
2023-07-27 09:30:21 +0200

    updated build tasks

--- tools/linux_build.py
+++ tools/linux_build.py
@@ -32,7 +32,7 @@ def stage2(map):
         txt = "-M{}".format(map)
     else:
         txt = ''
-    args = "bash tools/travis/dm.sh {} fortune13.dme".format(txt)
+    args = "bash tools/travis/dm.sh {} hailmary.dme".format(txt)
     print(args)
     p = subprocess.Popen(args, shell=True)
     wait(p)
