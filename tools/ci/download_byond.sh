4f1b10946a834fd7f55ff2ac42026b48f6212dfb
Tsurupeta <41485301+Tsurupeta@users.noreply.github.com.>
2026-01-21 11:33:50 +0100

    let's try this

--- tools/ci/download_byond.sh
+++ tools/ci/download_byond.sh
@@ -2,4 +2,4 @@
 set -e
 source dependencies.sh
 echo "Downloading BYOND version $BYOND_MAJOR.$BYOND_MINOR"
-curl "http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
+curl -H "User-Agent: Big Iron:Hail-Mary CI Script" "http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
