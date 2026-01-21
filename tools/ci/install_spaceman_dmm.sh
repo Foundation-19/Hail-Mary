cc79db4f6e42a42068334f99b22a488147d60156
Tsurupeta <41485301+Tsurupeta@users.noreply.github.com.>
2026-01-21 16:01:04 +0100

    no message

--- tools/ci/install_spaceman_dmm.sh
+++ tools/ci/install_spaceman_dmm.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 source dependencies.sh
 
-wget -O ~/dreamchecker "https://github.com/SpaceManiac/SpacemanDMM/releases/download/$SPACEMANDMM_TAG/dreamchecker"
+wget -O ~/dreamchecker "https://github.com/SpaceManiac/SpacemanDMM/releases/download/$SPACEMAN_DMM_VERSION/dreamchecker"
 chmod +x ~/dreamchecker
 
 ~/dreamchecker --version
