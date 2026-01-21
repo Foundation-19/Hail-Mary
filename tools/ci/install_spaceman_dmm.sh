84a43d9977a130a8f0d80f027165d2885c1574c1
Tsurupeta <41485301+Tsurupeta@users.noreply.github.com.>
2026-01-21 15:52:20 +0100

    no message

--- tools/ci/install_spaceman_dmm.sh
+++ tools/ci/install_spaceman_dmm.sh
@@ -3,17 +3,7 @@ set -euo pipefail
 
 source dependencies.sh
 
-if [ ! -f ~/$1 ]; then
-	mkdir -p "$HOME/SpacemanDMM"
-	CACHEFILE="$HOME/SpacemanDMM/$1"
+wget -O ~/dreamchecker "https://github.com/SpaceManiac/SpacemanDMM/releases/download/$SPACEMANDMM_TAG/dreamchecker"
+chmod +x ~/dreamchecker
 
-	if ! [ -f "$CACHEFILE.version" ] || ! grep -Fxq "$SPACEMAN_DMM_VERSION" "$CACHEFILE.version"; then
-		wget -O "$CACHEFILE" "https://github.com/SpaceManiac/SpacemanDMM/releases/download/$SPACEMAN_DMM_VERSION/$1"
-		chmod +x "$CACHEFILE"
-		echo "$SPACEMAN_DMM_VERSION" >"$CACHEFILE.version"
-	fi
-
-	ln -s "$CACHEFILE" ~/$1
-fi
-
-~/$1 --version
+~/dreamchecker --version
