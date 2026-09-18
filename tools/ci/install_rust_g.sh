ecaf5c9c4d2e24fdb0742109000e4ecbf363d625
Malerus <49745305+Malerus@users.noreply.github.com>
2026-09-17 23:40:52 -0500

    Update install_rust_g.sh

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 source dependencies.sh
 
 mkdir -p ~/.byond/bin
-wget -nv -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
+# retry on transient network/TLS handshake failures against the GitHub release CDN
+wget -nv --tries=5 --waitretry=5 --retry-connrefused -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
 chmod +x ~/.byond/bin/librust_g.so
 ldd ~/.byond/bin/librust_g.so
